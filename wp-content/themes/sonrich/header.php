a67853d11ff6c7c24683389a5f63803f99f68dea
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-12-08 13:55:22 +0530

    change title

--- wp-content/themes/sonrich/header.php
+++ wp-content/themes/sonrich/header.php
@@ -59,7 +59,7 @@
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/inc/responsive.css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
 
-    <title>Sonrich</title>
+    <title> Sonrich </title>
 	<?php wp_head(); ?>
 </head>
 
