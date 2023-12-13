710741449bd612ac1a20fc3dd1314664ace1e768
manojradeshan <manojradeshan2023@gmail.com>
2023-12-13 11:31:18 +0530

    good will page

---
+++ wp-content/themes/sonrich/page-good-will.php
@@ -0,0 +1,76 @@
+<?php
+/*
+Template Name: Default
+*/
+
+get_header();  ?>
+
+<?php get_template_part('template-parts/inner', 'banner'); ?>
+
+<!-- goodwill header start -->
+<section>
+    <div class="container py-5">
+        <div class="row">
+            <div class="col">
+                <h1 class="goodwill-heading text-center">
+                    Kindly share with us CSR project thats you have done and continuously going to do with
+                    images
+                </h1>
+            </div>
+        </div>
+        <div class="row pt-5">
+            <div class="col-12 col-md-4">
+                <p class="first-p about-p py-3">
+                    The visionaries behind Luvnarich believe that wealth is not the ultimate goal in life. They
+                    believe that true success is achieved when you use your resources to make a positive impact
+                    on the world.
+                </p>
+            </div>
+            <div class="col-12 col-md-4">
+                <p class="first-p about-p py-3">
+                    The visionaries behind Luvnarich believe that wealth is not the ultimate goal in life. They
+                    believe that true success is achieved when you use your resources to make a positive impact
+                    on the world.
+                </p>
+            </div>
+            <div class="col-12 col-md-4">
+                <p class="first-p about-p py-3">
+                    The luvnarich Foundation is committed to making a difference in the lives of others. They
+                    believe that everyone has the potential to make a positive impact on the world, and they are
+                    dedicated to providing the resources and support that people need to achieve their full
+                    potential.
+                </p>
+            </div>
+        </div>
+        <div class="goodwill-list-box px-2 px-md-5">
+            <div class="goodwill-container-title pt-5 fs-2 lh-sm pb-3">
+                Here are some of the ways that the luvnarich Foundation is making a difference:
+            </div>
+            <ul>
+                <li class="fs-4">
+                    Providing scholarships to students in need
+                </li>
+                <li class="fs-4">
+                    Supporting healthcare initiatives in developing countries
+                </li>
+                <li class="fs-4">
+                    Providing microloans to entrepreneurs
+                </li>
+                <li class="fs-4">
+                    Promoting environmental sustainability
+                </li>
+            </ul>
+            <div class="goodwill-p pt-5 fs-2 lh-sm pb-3">
+                The luvnarich Foundation is a force for good in the world. They are making a difference in the
+                lives of millions of people, and they are helping to create a better future for all.
+            </div>
+            <div class="goodwill-p pt-5 fs-2 lh-sm pb-3">
+                If you are interested in learning more about the luvnarich Foundation, please visit their
+                website or contact them today.
+            </div>
+        </div>
+    </div>
+</section>
+<!-- goodwill header end -->
+
+<?php get_footer(); ?>
