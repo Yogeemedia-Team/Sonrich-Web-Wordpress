1cd9da81f02d3c9bffe7437419503702f1b1103e
manojradeshan <manojradeshan2023@gmail.com>
2023-12-13 13:01:55 +0530

    private policy page

---
+++ wp-content/themes/sonrich/page-private-policy.php
@@ -0,0 +1,164 @@
+<?php
+/*
+Template Name: Default
+*/
+
+get_header();  ?>
+
+<?php get_template_part('template-parts/inner', 'banner'); ?>
+
+<!-- content section start -->
+<section class="content py-5">
+    <div class="container">
+        <div class="heading-box">
+            <div class="heading h2 font2 fs-1 text-center">
+                Privacy and Security Policy
+            </div>
+            <!-- <div class="sub-heading h5 font2 fs-3 text-center pt-2">
+                        Overview
+                    </div> -->
+        </div>
+        <div class="root-container-box">
+            <div class="root-container-title pt-5 fs-5">
+                <p>
+                    This privacy policy is for this website, LUVNARICH.net (hereinafter referred to as the
+                    "Site", "website") and served by LUVNARICH.net (hereinafter referred to as "us", "we",
+                    "our"),. and governs the privacy of its users who choose to use it, to assure the safeguard
+                    and preservation of your privacy and to comply with any applicable data protection and
+                    privacy laws.
+                </p>
+            </div>
+        </div>
+        <ol type="1">
+            <li class="py-3 fw-bold">The Information We Collect and How we use it.</li>
+            <p>
+                We collect information from you when you register on our site, place an order, subscribe to a
+                newsletter, respond to a survey and fill out a form or enter information on our site. This
+                information that we collect includes but is not limited to –
+            </p>
+            <ul>
+                <li>Your personal information you give us such as your name, address and email address</li>
+                <li>Information about your use of our Site including details of your visits such as pages
+                    viewed, the resources that you access and devices used to access the site. Such information
+                    includes traffic data, location data and other communication data.</li>
+                <li>Any other information you provide when you communicate with us by any means
+                    We may use the information in the following ways:</li>
+                <li>To personalize your experience and to allow us to deliver the type of content and product
+                    offerings in which you are most interested.</li>
+                <li>To improve our website in order to better serve you.</li>
+                <li>To allow us to better service you in responding to your customer service requests.</li>
+                <li>To administer a contest, promotion, survey or other site features.
+                <li>To quickly process your transactions.</li>
+                <li>To ask for ratings and reviews of services or products.</li>
+                <li>To follow up with inquiries after correspondence (live chat, email or phone inquiries)</li>
+                </li>
+            </ul>
+
+            <li class="py-3 fw-bold">Your Consent</li>
+            <p>
+                When you provide us with personal information to complete a transaction, verify your credit
+                card, place an order, arrange for a delivery or return a purchase, we imply that you consent to
+                our collecting it and using it for that specific reason only. If we ask for your personal
+                information for a secondary reason, like marketing, we will either ask you directly for your
+                expressed consent, or provide you with an opportunity to say no.
+            </p>
+
+            <li class="py-3 fw-bold">WEBXPAY</li>
+            <p>
+                Our store is enabled by WEBXPAY. They provide us with the online e-commerce platform that allows
+                us to sell our products and services to you. Your data is stored through WEBXPAY's data storage,
+                databases and the general WEBXPAY application. They store your data on a secure server behind a
+                firewall.
+            </p>
+
+            <li class="py-3 fw-bold">Security</li>
+            <p>
+                To protect your personal information, we take reasonable precautions and follow
+                industry best practices to make sure it is not inappropriately lost, misused,
+                accessed, disclosed, altered or destroyed. If you provide us with your credit card
+                information, all transactions are processed through a gateway provider and are not
+                stored or processed on our servers.
+            </p>
+
+            <li class="py-3 fw-bold">Cookies, pixels and other similar technologies</li>
+            <p>
+                Cookies and pixels are small pieces of data that a site or its service provider
+                transfers to your device's hard drive through your Web browser (if you allow) that
+                enables the site's or service provider's systems to recognize your browser and
+                capture and remember certain information. For instance, we use cookies to help us
+                remember and process the items in your shopping cart. They are also used to help
+                us understand your preferences based on previous or current site activity, which
+                enables us to provide you with improved services. We also use cookies to help us
+                compile aggregate data about site traffic and site interaction so that we can offer
+                better site experiences and tools in the future.
+                <br> We use cookies to:
+            </p>
+            <ul>
+                <li>Help remember and process the items in the shopping cart.</li>
+                <li>Understand and save user's preferences for future visits.</li>
+                <li>Keep track of advertisements.</li>
+                <li>Compile aggregate data about site traffic and site interactions in order to offer better
+                    site.</li>
+            </ul>
+            <p class="mt-3">
+                experiences and tools in the future. We may also use trusted third-party services
+                that track this information on our behalf.
+                You can choose to have your computer warn you each time a cookie is being sent,
+                or you can choose to turn off all cookies. You do this through your browser
+                settings. Since browser is a little different, look at your browser's Help Menu to
+                learn the correct way to modify your cookies. If you turn cookies off, some features
+                will be disabled. It won't affect the user's experience that make your site
+                experience more efficient and may not function properly. However, you will still be
+                able to place orders.
+            </p>
+
+            <li class="py-3 fw-bold">Third-party disclosure</li>
+            <p>
+                We do not sell, trade, or otherwise transfer to outside parties your information
+                unless we provide users with advance notice. This does not include website
+                hosting partners and other parties who assist us in operating our website,
+                conducting our business, or serving our users, so long as those parties agree to
+                keep this information confidential. We may also release information when it's
+                release is appropriate to comply with the law, enforce our site policies, or protect
+                ours or others' rights, property or safety. However, non-personally identiable
+                visitor information may be provided to other parties for marketing, advertising, or
+                other uses.
+            </p>
+
+            <li class="py-3 fw-bold">Third-party Services</li>
+            <p>
+                Occasionally, at our discretion, we may include or offer third-party products or
+                services on our website. In general, the third-party providers used by us will only
+                collect, use and disclose your information to the extent necessary to allow them to
+                perform the services they provide to us. However, certain third-party service
+                providers have their own privacy policies in respect to the information we are
+                required to provide to them. For these providers, we recommend that you read
+                their privacy policies so you can understand the manner in which your personal
+                information will be handled by these providers. Once you leave our store's website
+                or are redirected to a third-party website or application, you are no longer
+                governed by this Privacy Policy or our website's Terms of Service.
+            </p>
+
+            <li class="py-3 fw-bold">Changes to this privacy policy</li>
+            <p>
+                We reserve the right to modify this privacy policy at any time, so please review it
+                frequently. Changes and clarications will take effect immediately upon their
+                posting on the website. If our store is acquired or merged with another company,
+                your information may be transferred to the new owners so that we may continue to
+                sell products to you.
+            </p>
+
+            <li class="py-3 fw-bold">Contacting Us</li>
+            <p>
+                If there are any questions regarding this privacy policy, you may contact us using
+                the information below. <br>
+                LUVNARICH Pvt Ltd <br>
+                No. 204/A/1, Bandaragama Road, Kesbewa
+            </p>
+        </ol>
+    </div>
+</section>
+<!-- content section end -->
+
+
+<?php get_footer(); ?>
