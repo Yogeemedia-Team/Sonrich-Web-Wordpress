0e99dfc9bc3456b432a63984669bde4b5b56ca92
manojradeshan <manojradeshan2023@gmail.com>
2023-12-13 15:22:50 +0530

    delivery policy page

---
+++ wp-content/themes/sonrich/page-delivery-policy.php
@@ -0,0 +1,78 @@
+<?php
+/*
+Template Name: delivery policy
+*/
+
+get_header();  ?>
+
+<?php get_template_part('template-parts/inner', 'banner'); ?>
+
+<!-- content section start -->
+<section class="content py-5">
+    <div class="container">
+        <div class="heading-box">
+            <!-- <div class="heading h2 font2 fs-1 text-center">
+                        Preserve the our country today and for generations to come
+                    </div>
+                    <div class="sub-heading h5 font2 fs-3 text-center pt-2">
+                        kindly provide us the mission for the Luvanarich?
+                    </div> -->
+        </div>
+        <div class="root-container-box">
+            <div class="root-container-title pt-5 fs-5">
+                <p>
+                    We offer the following delivery options:
+                </p>
+                <ul>
+                    <li>Cash on Delivery.</li>
+                    <li>Payment through card.</li>
+                </ul>
+
+                <p>
+                    Kindly note that deliveries are not made on public holidays. Time slot cannot be specied
+                    with any of our delivery options. Please refer to the Terms and Conditions of Sales for
+                    further information.
+                </p>
+                <p>
+                    DELIVERY CHARGES AND ESTIMATED DELIVERY TIMES
+                </p>
+                <p>
+                    We deliver Island-wide.
+                </p>
+                <p>
+                    Delivery within Colombo 1-15 will be done within two days. (Please note that Delivery cut
+                    off time will be 4pm)
+                </p>
+                <p>
+                    Unfortunately deliveries to some postal addresses may not be available. If the delivery
+                    option to your address is not available, you will be notied about the same at the point of
+                    placing the order.
+                </p>
+                <p>
+                    You will be required to provide your signature to conrm the receipt at the point of
+                    delivery. If the product is not delivered due to any extrinsic circumstances, the parcel
+                    would be returned to our warehouse. Please reach us on
+                    <a href="mailto:info@luvnarich.net">info@luvnarich.net</a> or
+                    <a href="tel:+0753370000">0753370000</a> to arrange a redelivery and be informed that you
+                    may be responsible for additional delivery charges on any redelivery
+                </p>
+                <p>
+                    Delivery times are estimates only. We will try our best to meet the estimated delivery times
+                    but are not responsible for any delayed or unsuccessful deliveries.
+                </p>
+                <p>
+                    The orders may take longer and certain services may be removed during our seasonal sales,
+                    periods of promotional activities or due to circumstances beyond our control, such as
+                    adverse weather. If your delivery does not reach as expected, please contact us at
+                    <a href="mailto:info@luvnarich.net">info@luvnarich.net</a> or
+                    <a href="tel:+0753370000">0753370000</a>
+                </p>
+            </div>
+        </div>
+
+    </div>
+</section>
+<!-- content section end -->
+
+
+<?php get_footer(); ?>
