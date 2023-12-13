5e98f2d184e361faaee083fde1463a00fc6e93bf
manojradeshan <manojradeshan2023@gmail.com>
2023-12-13 14:17:50 +0530

    return policy page

---
+++ wp-content/themes/sonrich/page-return-policy.php
@@ -0,0 +1,96 @@
+<?php
+/*
+Template Name: return policy
+*/
+
+get_header();  ?>
+
+<?php get_template_part('template-parts/inner', 'banner'); ?>
+
+<!-- content section start -->
+<section class="content py-5">
+    <div class="container">
+        <div class="heading-box">
+            <!-- <div class="heading h2 font2 fs-1 text-center">
+                        Preserve the our country today and for generations to come
+                    </div>
+                    <div class="sub-heading h5 font2 fs-3 text-center pt-2">
+                        kindly provide us the mission for the Luvanarich?
+                    </div> -->
+        </div>
+        <div class="root-container-box">
+            <div class="root-container-title pt-5 fs-5">
+                <p class="fs-3 pb-3">
+                    Our policy lasts for 3 Days from the date of purchase. If 3 Days have gone by
+                    since your purchase, you shall not be offered a refund or an exchange.
+                </p>
+                <ul>
+                    <li>In order to be eligible for a return, the product must be unused and should be in the
+                        same
+                        condition that you received it. It must also be in the original packaging.</li>
+                    <li>A refund is applicable only for the following reasons specified:</li>
+                    <ul class="pt-1 pb-4">
+                        <li>Complete shipment arrives broken or damaged</li>
+                        <li>Product does not match with the key features in online description</li>
+                        <li>Product is sent in the wrong style or quantity</li>
+                        <li>Partial shipment arrives damaged or broken</li>
+                    </ul>
+
+                    <li>Any hidden penalty fees, surcharges or return shipping fees will not be charged.</li>
+                    <li>To complete your return, we require a receipt or proof of purchase.</li>
+                    <li>Please do not send your purchase back to the manufacturer.</li>
+                    <li>There are certain instances where only partial refunds are granted. Contact us for
+                        further information.</li>
+                    <li>Change of mind is not allowed in any circumstances.</li>
+                </ul>
+
+                <div class="policy-title fw-bold pt-3">Refunds</div>
+                <p class="px-2 px-md-5">
+                    Once your return is received and inspected, you will be notified about the same. You will
+                    also be notified of the approval or rejection of your refund. If you are approved, then your
+                    refund will be processed, and your credit card will automatically be credited or you will be
+                    refunded in the original method of payment, within a certain amount of days.
+                </p>
+
+                <div class="policy-title fw-bold pt-3">Late Payments</div>
+                <p class="px-2 px-md-5">
+                    If you haven't received a refund yet, re check your bank account. Thereafter
+                    contact your credit card issuer or bank; it may take some time for your
+                    refund to be officially reflected due to the processing time. If still not
+                    refunded, please contact us at <a href="tel:+0753370000">0753370000</a> or
+                    <a href="mailto:info@luvnarich.net">info@luvnarich.net</a>
+                </p>
+
+                <div class="policy-title fw-bold pt-3">Sale items</div>
+                <p class="px-2 px-md-5">
+                    Only regular priced items may be refunded, unfortunately sale items cannot be refunded.
+                </p>
+
+                <div class="policy-title fw-bold pt-3">Exchanges</div>
+                <p class="px-2 px-md-5">
+                    Exchanges may be possible if the product is still in its original condition,
+                    packaging and tags are still intact. We only replace items if they are
+                    defective or damaged. If you have any queries on a possible exchange or
+                    you require an exchange, please contact us at <a href="tel:+0753370000">0753370000</a> or
+                    <a href="mailto:info@luvnarich.net">info@luvnarich.net</a>
+                </p>
+
+                <div class="policy-title fw-bold pt-3">Shipping</div>
+                <p class="px-2 px-md-5">
+                    To return your product, you should mail your product to <strong>No:204/A/1, Bandaragama Road,
+                        Kesbewa.</strong> You will be responsible to pay for the shipping costs while returning your
+                    product.
+                    Shipping costs are non-refundable. If you receive a refund, the cost of return shipping will
+                    be deducted from your refund. Depending on your location, the time exchanged product to
+                    reach you, may vary.
+                </p>
+
+            </div>
+        </div>
+
+    </div>
+</section>
+<!-- content section end -->
+
+
+<?php get_footer(); ?>
