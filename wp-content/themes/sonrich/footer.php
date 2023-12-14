e431df627295647ee1caa22b24902d04867c9b9e
manojradeshan <manojradeshan2023@gmail.com>
2023-12-14 11:49:21 +0530

    new footer

--- wp-content/themes/sonrich/footer.php
+++ wp-content/themes/sonrich/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -12,80 +13,165 @@
 ?>
 
 <!-- footer Section Start -->
-<footer>
+<!-- <footer>
     <div class="footer-section">
         <div class="row justify-content-center flex-wrap text-center mt-4">
             <div class="col-sm-12 col-md-6 col-lg-6 footer-top-left text-center text-md-end">
                 <a href="index.html">
-                    <img src="<?php echo get_template_directory_uri(); ?>/inc/images/sonrich_ashia_logo.png" alt="footer logo" class="footer-logo">
+                    <img src="<?php echo get_template_directory_uri(); ?>/inc/images/sonrich_ashia_logo.png"
+                        alt="footer logo" class="footer-logo">
                 </a>
             </div>
-                <div class="col-sm-12 col-md-6 col-lg-6 footer-top-right text-center text-md-start mt-0 mt-lg-3">
-                    <i class="fa fa-brands fa-facebook-f text-light fs-2 px-2"></i>
-                    <i class="fa-brands fa-linkedin-in text-light fs-2 px-2"></i>
-                    <a href="#" class="footer-button btn align-bottom ms-3">Click here to Apply</a>
+            <div class="col-sm-12 col-md-6 col-lg-6 footer-top-right text-center text-md-start mt-0 mt-lg-3">
+                <i class="fa fa-brands fa-facebook-f text-light fs-2 px-2"></i>
+                <i class="fa-brands fa-linkedin-in text-light fs-2 px-2"></i>
+                <a href="#" class="footer-button btn align-bottom ms-3">Click here to Apply</a>
+            </div>
+        </div>
+        <div class="footer-last mt-4">
+            <div class="row text-light">
+                <div class="col-sm-12 col-md-6 col-lg-6 text-center text-md-end">
+                    <p class="text-center text-md-end w-100">2022 Sonrich Asia(Pvt) Ltd. All Right Reserved</p>
                 </div>
+                <div class="col-sm-12 col-md-6 col-lg-6 text-center text-md-start mt-3 mt-md-0">
+                    <p class="text-center text-md-start w-100">
+                        No.204/A/1,Bandaragama Road,Kesbewa,SriLanka
+                    </p>
                 </div>
-                <div class="footer-last mt-4">
-                    <div class="row text-light">
-                        <div class="col-sm-12 col-md-6 col-lg-6 text-center text-md-end">
-                            <p class="text-center text-md-end w-100">2022 Sonrich Asia(Pvt) Ltd. All Right Reserved</p>
-                        </div>
-                        <div class="col-sm-12 col-md-6 col-lg-6 text-center text-md-start mt-3 mt-md-0">
-                            <p class="text-center text-md-start w-100">
-                                No.204/A/1,Bandaragama Road,Kesbewa,SriLanka
-                            </p>
-                        </div>
+            </div>
+        </div>
+    </div>
+</footer> -->
+
+<footer>
+    <section class="footer-section">
+        <div class="container">
+            <div class="row py-5">
+                <div class="col-12 col-md-6 d-flex justify-content-center align-items-center">
+                    <a href="index.html">
+                        <img src="<?php echo get_template_directory_uri(); ?>/inc/Images/sonrich_logo.png"
+                            alt="footer logo" class="footer-logo w-100">
+                    </a>
+                </div>
+                <div class="col-12 col-md-3">
+                    <ul>
+                        <h3 class="footer-link-title fw-bold py-3 text-light">About Us</h3>
+                        <li class="py-2 text-light" style="list-style: none;">
+                            Just a short sentence. This is just a demo text you should overwrite.
+                        </li>
+                        <li class="py-2 text-light" style="list-style: none;">
+                            No.204/A/1,Bandaragama Road,Kesbewa,SriLanka
+                        </li>
+                        <li class="py-2 text-light" style="list-style: none;"></li>
+                        <a href="#" target="_blank" style="text-decoration: none;">
+                            <i class="fa-brands fa-facebook text-light fs-4 p-2"></i>
+                        </a>
+                        <a href="#" target="_blank" style="text-decoration: none;">
+                            <i class="fa-brands fa-twitter text-light fs-4 p-2"></i>
+                        </a>
+                        <a href="#" target="_blank" style="text-decoration: none;">
+                            <i class="fa-brands fa-youtube text-light fs-4 p-2"></i>
+                        </a>
+                        <a href="#" target="_blank" style="text-decoration: none;">
+                            <i class="fa-brands fa-tiktok text-light fs-4 p-2"></i>
+                        </a>
+                    </ul>
+                </div>
+                <div class="col-12 col-md-3">
+                    <ul>
+                        <h3 class="footer-link-title fw-bold py-3 text-light">Theme Info</h3>
+                        <li class="py-1 text-light" style="list-style: none;">
+                            <a href="#" class="text-light" style="text-decoration: none;">
+                                policy
+                            </a>
+                        </li>
+                        <li class="py-1 text-light" style="list-style: none;">
+                            <a href="#" class="text-light" style="text-decoration: none;">
+                                policy
+                            </a>
+                        </li>
+                        <li class="py-1 text-light" style="list-style: none;">
+                            <a href="#" class="text-light" style="text-decoration: none;">
+                                policy
+                            </a>
+                        </li>
+                        <li class="py-1 text-light" style="list-style: none;">
+                            <a href="#" class="text-light" style="text-decoration: none;">
+                                policy
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="back-to-top float-end">
+                        <a href="#top" class="text-light">
+                            <i class="fa-solid fa-arrow-up fs-2 py-1 px-2 border rounded"></i>
+                        </a>
+                    </div>
+                    <div class="copyright-text">
+                        <p class="text-light text-start text-md-center">
+                            <script>
+                            document.write(new Date().getFullYear())
+                            </script>
+                            Sonrich Asia(Pvt) Ltd. All Right Reserved
+                        </p>
                     </div>
                 </div>
             </div>
-        </footer>
-        <!-- Footer Section End -->
-    </div>
+        </div>
+    </section>
+</footer>
 
-    <!-- main js -->
-    <script src="inc/js/script.js"></script>
+<!-- Footer Section End -->
+</div>
 
-    <!-- bootstrap js -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<!-- main js -->
+<script src="inc/js/script.js"></script>
 
-    <!-- jQuery -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"
+<!-- bootstrap js -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
+
+<!-- jQuery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"
     integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw=="
     crossorigin="anonymous" referrerpolicy="no-referrer">
-    </script>
-
-    <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            var banner = document.querySelector('.parallax-banner-wrapper');
-
-            window.addEventListener('scroll', function () {
-                var scrolled = window.scrollY;
-                var translateY = scrolled * 0.4; // Adjust the parallax speed as needed
-
-                banner.querySelector('.parallax-banner-image').style.transform = 'translate3d(0, ' + translateY + 'px, 0)';
-            });
-        });
-    </script>
+</script>
 
 <script>
+document.addEventListener('DOMContentLoaded', function() {
+    var banner = document.querySelector('.parallax-banner-wrapper');
 
-jQuery(document).ready(function(){
+    window.addEventListener('scroll', function() {
+        var scrolled = window.scrollY;
+        var translateY = scrolled * 0.4; // Adjust the parallax speed as needed
+
+        banner.querySelector('.parallax-banner-image').style.transform = 'translate3d(0, ' +
+            translateY + 'px, 0)';
+    });
+});
+</script>
+
+<script>
+jQuery(document).ready(function() {
     // This button will increment the value
-    $('.qtyplus').click(function(e){
+    $('.qtyplus').click(function(e) {
         // Stop acting like a button
         e.preventDefault();
         // Get the field name
         fieldName = $(this).attr('field');
         // Get its current value
-        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        var currentVal = parseInt($('input[name=' + fieldName + ']').val());
         // If is not undefined
         if (!isNaN(currentVal)) {
             // Increment
-            $('input[name='+fieldName+']').val(currentVal + 1);
+            $('input[name=' + fieldName + ']').val(currentVal + 1);
         } else {
             // Otherwise put a 0 there
-            $('input[name='+fieldName+']').val(0);
+            $('input[name=' + fieldName + ']').val(0);
         }
     });
     // This button will decrement the value till 0
@@ -95,21 +181,21 @@ jQuery(document).ready(function(){
         // Get the field name
         fieldName = $(this).attr('field');
         // Get its current value
-        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        var currentVal = parseInt($('input[name=' + fieldName + ']').val());
         // If it isn't undefined or its greater than 0
         if (!isNaN(currentVal) && currentVal > 0) {
             // Decrement one
-            $('input[name='+fieldName+']').val(currentVal - 1);
+            $('input[name=' + fieldName + ']').val(currentVal - 1);
         } else {
             // Otherwise put a 0 there
-            $('input[name='+fieldName+']').val(0);
+            $('input[name=' + fieldName + ']').val(0);
         }
     });
 });
-
 </script>
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
