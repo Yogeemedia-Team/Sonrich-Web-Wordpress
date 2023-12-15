041e6e0e6f2935f26e99fba5f76673bcf555eee9
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-12-15 13:39:34 +0530

    Update inner-banner.php

--- wp-content/themes/sonrich/template-parts/inner-banner.php
+++ wp-content/themes/sonrich/template-parts/inner-banner.php
@@ -1,15 +1,25 @@
 <div class="main-wrapper">
         <!-- parallax banner is start -->
         <div class="parallax-banner-wrapper">
-            <h1 id="banner-header-text" class="parallax-banner-text-box fw-normal">Product</h1>
-            <?php 
+           <?php 
+				if (wp_get_attachment_url( get_post_thumbnail_id($post->ID) ) )
+				  {
+					$bannerUrl = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+				  }
+				
+				else{
+          $banner = get_field('default_inner_banner', 'option');
+         $bannerUrl = get_template_directory_uri() . '/inc/images/product_banner.jpg';
+
+				}
+ 
                      if ( is_front_page() ) : ?>
                        <h1 id="banner-header-text" class="parallax-banner-text-box fw-normal">Product</h1>
                     <?php else : ?>
                         <h1 id="banner-header-text" class="parallax-banner-text-box fw-normal"><?php the_title()?></h1>
                     <?php endif; 
                 ?>
-            <div class="parallax-banner-image" style="background-image: url(<?php echo get_template_directory_uri(); ?>/inc/images/product_banner.jpg);">
+            <div class="parallax-banner-image" style="background-image: url(<?php echo $bannerUrl; ?>);">
             </div>
         </div>
         <!-- parallax banner is end -->
