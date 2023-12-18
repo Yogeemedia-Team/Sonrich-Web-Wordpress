252b419bc93f1d214d4b76b4d9d67570f22c8030
manojradeshan <manojradeshan2023@gmail.com>
2023-12-18 10:33:50 +0530

    product gallery js add

--- wp-content/themes/sonrich/footer.php
+++ wp-content/themes/sonrich/footer.php
@@ -199,6 +199,21 @@ jQuery(document).ready(function() {
 });
 </script>
 
+<script>
+const image = document.querySelector(".hero img");
+
+const reset = () => image.classList.remove("fadeIn");
+
+function activate(e) {
+    if (!e.target.matches(".btns img")) return;
+    image.src = e.target.src;
+    image.classList.add("fadeIn");
+}
+
+document.addEventListener("click", activate, false);
+document.addEventListener("animationend", reset, false);
+</script>
+
 <?php wp_footer(); ?>
 
 </body>
